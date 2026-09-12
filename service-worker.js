4c31c515cc7d36f741ee45403afb75e28bb53241
Oscar Salazar <salazarssjblue@gmail.com>
2026-09-11 20:09:30 -0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Service Worker para VICEL Finanzas
 // ============================================================
 
-const CACHE_NAME = 'vicel-finanzas-v3';
+const CACHE_NAME = 'vicel-finanzas-v4';
 
 const PRECACHE_URLS = [
   './',
